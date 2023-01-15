63981d9a65f0a954554ea83503b78366ca1a0a9e
Lahiru Thennakoon <lahiruthennakoon@Lahirus-Macbook-pro-15.local>
2023-01-15 15:56:31 +0530

    add tags

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,6 +3,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features ="/Users/lahiruthennakoon/IdeaProjects/SeleniumAutomationFrameworkWithBDD/src/test/java/Features",plugin = {"pretty","html:target/htmlReports/report.html"})
+@CucumberOptions(features ="/Users/lahiruthennakoon/IdeaProjects/SeleniumAutomationFrameworkWithBDD/src/test/java/Features",
+        plugin = {"pretty","html:target/htmlReports/report.html"},
+        tags = "@smoke"
+)
 public class TestRunner {
 }
--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -1,4 +1,5 @@
 Feature: Feature to test login functionality
+
   Scenario: Check login is successful with valid credentials
 
     Given user is on login page
